771b4fa94e10600e91a026b23a8d0403b66c9cfc
Sarah S <SarahS@Sarahs-MacBook-Pro.local>
2019-08-25 20:53:18 +0300

    file complated

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DR.db')
+df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/clf_cv.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
